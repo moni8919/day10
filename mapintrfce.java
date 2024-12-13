f0fd1fa95737cf2a3e58edb647435782ce0a4581
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 14:45:40 +0530

    Add files via upload

---
+++ mapintrfce.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class mapintrfce {
+    public static void main(String[] args){
+        HashMap<String,String>a=new HashMap<>();
+        a.put("SNO","01");
+        a.put("SName","muraja");
+        System.out.println(a);
+
+    }
+    
+}
