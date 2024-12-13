2b1b89f0e47bedece175defd108ed0e0c6402689
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 13:59:55 +0530

    Add files via upload

---
+++ str3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+  public class str3{
+    public static void main(String args[]){
+        String a1[]={"hello","manisha","mounika"};
+        String a2[]={"hello","hi","hasini","mounika"};
+        Set<String> a=new HashSet<>();
+        Set<String> b=new HashSet<>();
+        for(String s:a1)
+        a.add(s);
+        for(String s1:a2)
+        b.add(s1);
+        System.out.println(a);
+        System.out.println(b);
+        Set<String> u=new HashSet<>(a);
+        u.removeAll(b);
+        System.out.println(u);
+
+    }
+    
+}
