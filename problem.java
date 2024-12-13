586ab69576d7829a4a1cdbe8349be5f683aca35b
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:04:19 +0530

    Add files via upload

---
+++ problem.java
@@ -0,0 +1,19 @@
+ import java.util.*;
+ public class problem {
+    public static void main(String[] args){
+        int arr[]={77,8,9,5,6,77,8,99,8};
+        HashMap<Integer,Integer>a=new HashMap<>();
+        for(int a1:arr){
+            if(a.containsKey(01))
+            {
+                a.put(a1,a.get(a1)+1);
+            }else{
+                a.put(a1,1);
+
+            }
+        }
+        System.out.println(a);
+
+    }
+    
+}
