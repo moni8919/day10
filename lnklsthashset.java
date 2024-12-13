da66d59ba20e7e78f86ca03510bf6fe5c265975b
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 12:01:57 +0530

    Add files via upload

---
+++ lnklsthashset.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class lnklsthashset {
+    public static void main(String[] args){
+        LinkedHashSet<String>a=new LinkedHashSet<>();
+        a.add("hello");
+        a.add("hello");
+        a.add("students");
+        a.add("rajesh");
+        a.add("students");
+        System.out.print(a);
+    }
+    
+}
